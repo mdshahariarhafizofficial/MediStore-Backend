c4b6f2c35d0bea6d47f0a73bf34045108d929710
mdshahariarhafizofficial <shahariar.works@gmail.com>
2026-01-29 21:41:35 +0600

    validation middleware added

---
+++ src/middleware/validate.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const validate = (schema: any) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync({
+        body: req.body,
+        query: req.query,
+        params: req.params
+      });
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
