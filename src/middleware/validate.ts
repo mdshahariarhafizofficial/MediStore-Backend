56b75241a520ce89bb83573f7726605f5540c4bb
mdshahariarhafizofficial <shahariar.works@gmail.com>
2026-02-01 14:20:31 +0600

    fix login/register issue

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,13 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
+import { ZodObject } from 'zod';
 
-export const validate = (schema: any) => {
+export const validate = (schema: ZodObject) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      await schema.parseAsync({
-        body: req.body,
-        query: req.query,
-        params: req.params
-      });
+      req.body = await schema.parseAsync(req.body);
       next();
     } catch (error) {
       next(error);
